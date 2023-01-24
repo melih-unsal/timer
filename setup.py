fe3e2b02227cdc14245bdded1b7d6d514f000dbd
melih1996 <06melihunsal@gmail.com>
2023-01-24 18:04:28 +0300

    fix error in setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='toolbox',
-    version='0.0.3',
-    author='Mike Huls',
-    author_email='mlih@synthdata.co',
-    description='Testing installation of Package',
+    name='timer',
+    version='0.0.1',
+    author='Melih Unsal',
+    author_email='melih@synthdata.co',
+    description='Timer Package',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/mike-huls/toolbox',
+    url='https://github.com/melih1996/timer.git',
     license='MIT',
-    packages=['toolbox'],
-    install_requires=['requests'],
+    packages=['timer'],
+    install_requires=[],
 )
