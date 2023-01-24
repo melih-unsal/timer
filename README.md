548aab9ea404e9f86973eec03d2e882f3a439a84
melih1996 <06melihunsal@gmail.com>
2023-01-24 18:00:59 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Timing info for a function.
---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='toolbox',
+    version='0.0.3',
+    author='Mike Huls',
+    author_email='mlih@synthdata.co',
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/mike-huls/toolbox',
+    license='MIT',
+    packages=['toolbox'],
+    install_requires=['requests'],
+)
